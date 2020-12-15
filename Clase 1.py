531ec680f38dcf7ce6044a8ae67e47c294e53e68
JuBastian <53878806+JuBastian@users.noreply.github.com>
2020-12-14 19:02:22 -0500

    My first commit

---
+++ Clase 1.py
@@ -0,0 +1,4 @@
+print("Input a number ")  # Replace the quote marks here, for some reason they get pasted wrong
+number = int(input())
+print("Your number plus 3:")
+print(3 + number)
---
+++ Let's practice - 1.py
@@ -0,0 +1,6 @@
+print("Please input the number of elements ")
+number_of_elements = int(input())
+print("Please input the cost of the element ")
+cost_per_element = float(input())
+total_cost = number_of_elements * cost_per_element
+print(total_cost)
