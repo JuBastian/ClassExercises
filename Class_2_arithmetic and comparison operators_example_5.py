38e62c45111639fab722d34e5c816d189eb82f9b
JuBastian <53878806+JuBastian@users.noreply.github.com>
2020-12-15 20:09:49 -0500

    Create Class_2_arithmetic and comparison operators_example_5.py

---
+++ Class_2_arithmetic and comparison operators_example_5.py
@@ -0,0 +1,6 @@
+print("Please input the number of elements ")
+Elements = int(input())
+print("Please input the character for your banner ")
+banner_character = str(input())
+Banner = Elements * banner_character
+print(Banner + "Banner" + Banner)
